e8a3dcd423547bb7a4e8e1c3d8568cc530ec736e
Crowdbotics <team@crowdbotics.com>
2023-06-19 17:51:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-snow-41600",
+  url: "https://orange-snow-41600.botics.co",
+  api: "https://orange-snow-41600.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
